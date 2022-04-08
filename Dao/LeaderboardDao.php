228950609349ca22172937e305beb937b4e719c5
andrewniemann14 <76855278+andrewniemann14@users.noreply.github.com>
2022-04-07 21:38:42 -0500

    new search option

--- Dao/LeaderboardDao.php
+++ Dao/LeaderboardDao.php
@@ -5,29 +5,36 @@ require_once PROJECT_ROOT_PATH . "/Dao/Dao.php";
 
 class LeaderboardDao extends Dao {
 
-  // /entries?name=8ourne
+  // /leaderboard?name=8ourne
   public function getAllPersonals($name) {
     return $this->select("SELECT * FROM challenges INNER JOIN leaderboard ON leaderboard.challenge_id = challenges.id WHERE name = ?", ["s", $name]);
   }
 
-  // /entries?name=8ourne?limit=20
+  // /leaderboard?name=8ourne?limit=20
   public function getRecentPersonals($name, $limit) {
     return $this->select("SELECT * FROM challenges INNER JOIN leaderboard ON leaderboard.challenge_id = challenges.id WHERE name = ? ORDER BY id DESC LIMIT ?", ["si", $name, $limit]);
   }
 
-  // /entries?name=8ourne?stage=blahblah?class=eRallyR5Caps
+  // /leaderboard?name=8ourne?stage=blahblah?class=eRallyR5Caps
   public function getFastestPersonalForStageAndClass($name, $stage, $class) {
     return $this->select("SELECT * FROM challenges INNER JOIN leaderboard ON leaderboard.challenge_id = challenges.id WHERE name = ? AND stage = ? AND vehicle_class = ? ORDER BY time ASC LIMIT 1", ["sss", $name, $stage, $class]);
   }
 
-  // /entries?name=8ourne?stage=blahblah?limit=10
+  // /leaderboard?name=8ourne?stage=blahblah?limit=10
   public function getFastestPersonalsForStage($name, $stage, $limit) {
     return $this->select("SELECT * FROM challenges INNER JOIN leaderboard ON leaderboard.challenge_id = challenges.id WHERE name = ? AND stage = ? ORDER BY time ASC LIMIT ?", ["ssi", $name, $stage, $limit]);
   }
 
 
-  // /entries?stage=blahblah?class=eR5C?limit=10
+  // /leaderboard?stage=blahblah?class=eR5C?limit=10
   public function getFastestGlobalsForStageAndClass($stage, $class, $limit) {
     return $this->select("SELECT * FROM challenges INNER JOIN leaderboard ON leaderboard.challenge_id = challenges.id WHERE stage = ? AND vehicle_class = ? ORDER BY time ASC LIMIT ?", ["ssi", $stage, $class, $limit]);
   }
+
+  // /leaderboard?id=123456
+  public function getFullChallenge($id) {
+    return $this->select("SELECT * FROM leaderboard WHERE challenge_id = ?", ["i", $id]);
+  }
+
+
 }
--- index.php
+++ index.php
@@ -32,7 +32,7 @@ if (isset($uri[3])) {
   $methodName = 'control' . ucfirst($uri[3]); // controlLeaderboard()
   $controller->{$methodName}();
 } else {
-  echo '<b>Possible queries:</b><br/>/challenges?limit=2<br/>/leaderboard?name=8ourne<br/>/leaderboard?name=8ourne?limit=100<br/>/leaderboard?name=8ourne?stage=blahblah?class=eRallyR5Caps<br/>/leaderboard?name=8ourne?stage=blahblah?limit=10<br/>/leaderboard?stage=blahblah?class=eRallyR5Caps?limit=10<br/>';
+  echo '<b>Possible queries:</b><br/>/challenges?limit=2<br/>/leaderboard?id=123456<br/>/leaderboard?name=8ourne<br/>/leaderboard?name=8ourne?limit=100<br/>/leaderboard?name=8ourne?stage=blahblah?class=eRallyR5Caps<br/>/leaderboard?name=8ourne?stage=blahblah?limit=10<br/>/leaderboard?stage=blahblah?class=eRallyR5Caps?limit=10<br/>';
 }
 
 ?>
